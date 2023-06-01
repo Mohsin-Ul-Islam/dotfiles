ad6fd68d8bec0f46575df6b24ba3651f08270569
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-06-01 21:15:26 +0500

    feat: add format on save, alt+j, alt+k move lines functionality

--- .config/nvim/lua/aqua/remap.lua
+++ .config/nvim/lua/aqua/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", { noremap = true })
+
+vim.api.nvim_set_keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true })
+vim.api.nvim_set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true })
