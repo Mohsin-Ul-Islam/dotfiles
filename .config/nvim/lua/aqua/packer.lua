a0ad2d4e31266245c4a29ccbd9ce919e2706a895
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-07-09 14:56:16 +0500

    fix: line 14 in packer.nvim

--- .config/nvim/lua/aqua/packer.lua
+++ .config/nvim/lua/aqua/packer.lua
@@ -4,34 +4,35 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use 'navarasu/onedark.nvim'
-  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
-  use 'ThePrimeagen/harpoon'
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use 'mattn/emmet-vim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use 'navarasu/onedark.nvim'
+    use 'ThePrimeagen/harpoon'
+    use { 'nvim-treesitter/nvim-treesitter', run = { ':TSUpdate' } }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
-
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    }
 end)
