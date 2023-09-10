245fd34b30b0137aa7fcc70c347b1ae142259710
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-09-10 21:38:01 +0300

    feat: add bazel support for gopls

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -4,7 +4,23 @@ lsp.on_attach(function(_, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
 end)
 
+
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').gopls.setup({
+    settings = {
+        gopls = {
+            env = {
+                GOPACKAGESDRIVER = './tools/gopackagesdriver.sh'
+            },
+            directoryFilters = {
+                "-bazel-bin",
+                "-bazel-out",
+                "-bazel-testlogs",
+                "-bazel-cigar",
+            },
+        },
+    },
+})
 
 lsp.setup()
 
