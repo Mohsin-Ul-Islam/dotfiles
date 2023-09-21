690964a133c813d15c2a4a9d9fa72b03f24be4bc
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-09-21 12:38:57 +0300

    feat(nvim): add mappings for yanking to system clipboard

--- .config/nvim/lua/aqua/remap.lua
+++ .config/nvim/lua/aqua/remap.lua
@@ -8,3 +8,7 @@ vim.api.nvim_set_keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true })
 vim.api.nvim_set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true })
 
 vim.api.nvim_set_keymap("n", "<leader>sd", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>yy", '"+yy', { noremap = true })
