a9f80f5a44c309d58a96768131667333aeee03ab
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-09-22 02:41:11 +0300

    feat(nvim): add autocompletion for database integration

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -7,3 +7,18 @@ cmp.setup({
         ['<C-k>'] = cmp.mapping.select_prev_item(),
     }
 })
+
+local autocomplete_group = vim.api.nvim_create_augroup("vimrc_autocompletion", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "sql", "mysql", "plsql" },
+    callback = function()
+        cmp.setup.buffer({
+            sources = {
+                { name = "vim-dadbod-completion" },
+                { name = "buffer" },
+                { name = "luasnip" },
+            },
+        })
+    end,
+    group = autocomplete_group,
+})
