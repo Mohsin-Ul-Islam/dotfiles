433763b48afd4be66189e9c7284a9aae05040af7
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-09-22 03:02:42 +0300

    fix(nvim): cmp only if the menu is visible

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -3,9 +3,17 @@ local cmp = require('cmp')
 cmp.setup({
     mapping = {
         ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        ['<C-j>'] = cmp.mapping.select_next_item(),
-        ['<C-k>'] = cmp.mapping.select_prev_item(),
-    }
+        ['<C-j>'] = cmp.mapping(function(callback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            end
+        end),
+        ['<C-k>'] = cmp.mapping(function(callback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            end
+        end),
+    },
 })
 
 local autocomplete_group = vim.api.nvim_create_augroup("vimrc_autocompletion", { clear = true })
