2bb68576ac55e97f9bee13125971109d0d8167b6
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-11-02 11:26:42 +0300

    fix(nvim): add gitsigns opts field to imply calling setup

--- .config/nvim/lua/aqua/plugins/gitsigns.lua
+++ .config/nvim/lua/aqua/plugins/gitsigns.lua
@@ -1,5 +1,6 @@
 return {
     'lewis6991/gitsigns.nvim',
+    opts = {},
     keys = {
         { "<leader>nh", "<cmd>lua require('gitsigns').next_hunk()<CR>" },
         { "<leader>ph", "<cmd>lua require('gitsigns').prev_hunk()<CR>" },
