a13629ad9e6fe88beaef32fc5df84c4bfbd88f1b
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-11-05 16:20:24 +0300

    feat: add code actions mapping & reset hunk mapping

--- .config/nvim/lua/aqua/plugins/gitsigns.lua
+++ .config/nvim/lua/aqua/plugins/gitsigns.lua
@@ -4,5 +4,6 @@ return {
     keys = {
         { "<leader>nh", "<cmd>lua require('gitsigns').next_hunk()<CR>" },
         { "<leader>ph", "<cmd>lua require('gitsigns').prev_hunk()<CR>" },
+        { "<leader>rh", "<cmd>lua require('gitsigns').reset_hunk()<CR>" },
     },
 }
--- .config/nvim/lua/aqua/remap.lua
+++ .config/nvim/lua/aqua/remap.lua
@@ -7,8 +7,9 @@ vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", { noremap = true })
 vim.api.nvim_set_keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true })
 vim.api.nvim_set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true })
 
-vim.api.nvim_set_keymap("n", "<leader>sd", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>rs", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>sd", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true })
