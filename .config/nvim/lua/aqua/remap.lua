1ffad1b545403ac3f6bc53da5c1dce27d0c19e30
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-11-08 14:45:38 +0300

    feat: add diagnostics next & previous mappings

--- .config/nvim/lua/aqua/remap.lua
+++ .config/nvim/lua/aqua/remap.lua
@@ -9,7 +9,11 @@ vim.api.nvim_set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true })
 
 vim.api.nvim_set_keymap("n", "<leader>rs", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
+
+-- code diagnostics
 vim.api.nvim_set_keymap("n", "<leader>sd", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>nd", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>pd", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true })
