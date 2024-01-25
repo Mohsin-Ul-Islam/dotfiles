fc1f33aa9e648d5d3ee6aabd14b992846e03f7b3
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2024-01-25 11:08:31 +0300

    feat: add injections for UPSERT statements

--- .config/nvim/queries/go/injections.scm
+++ .config/nvim/queries/go/injections.scm
@@ -2,6 +2,6 @@
    (interpreted_string_literal)
    (raw_string_literal)
  ] @sql
- (#match? @sql "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
+ (#match? @sql "(SELECT|select|INSERT|insert|UPSERT|upsert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
  (#offset! @sql 0 1 0 -1))
 
