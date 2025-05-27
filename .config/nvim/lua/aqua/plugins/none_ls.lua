e79e796c2a6072b3308147609a95d9f4448c8cb6
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2025-05-27 11:36:38 +0300

    feat(plugin): make isort compatible with black formatting

--- .config/nvim/lua/aqua/plugins/none_ls.lua
+++ .config/nvim/lua/aqua/plugins/none_ls.lua
@@ -7,9 +7,15 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.isort,
                 null_ls.builtins.diagnostics.mypy,
                 null_ls.builtins.formatting.stylua,
+
+                -- setup isort
+                null_ls.builtins.formatting.isort.with({
+                    extra_args = { "--profile", "black" }
+                }),
+
+                -- setup pylint
                 null_ls.builtins.diagnostics.pylint.with({
                     env = function(params)
                         return { PYTHONPATH = params.root }
